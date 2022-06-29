782622607826b51e50de6b47fd691e564975aefb
Eugene Menski <eugene.menski@gmail.com>
2022-06-29 11:20:08 +0300

    Added inputArray method

--- Program.cs
+++ Program.cs
@@ -7,3 +7,44 @@
 ["1234", "1567", "-2", "computer science"] --> ["-2"]
 ["Russia", "Denmark", "Kazan"] --> []*/
 
+void NextElementMessage(int number)
+{
+    Console.Write($"Введите {number}-й элемент: ");
+}
+
+string GetNextElement()
+{
+    return Console.ReadLine();
+}
+
+string[] InputArray()
+{ 
+    int i = 0;
+
+    string[] input = new string[1];
+    
+    bool getNext = true;
+
+    do
+    {
+        NextElementMessage(i + 1);
+
+        Array.Resize(ref input, i + 1);
+
+        input[i] = GetNextElement();
+    
+        if(input[i] == "")
+        {
+            getNext = false;
+        }
+
+        i = i + 1;
+    }
+    while(getNext);
+
+    return input;
+}
+
+Console.WriteLine("Введите исходный массив строк поэлементно. Для выхода оставьте строку ввода пустой и нажмите 'Enter'.");
+string[] inputArray = InputArray();
+Console.WriteLine("Ввод завершен!");
